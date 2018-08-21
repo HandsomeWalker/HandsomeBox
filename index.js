6108d270ef60d3fee0de92eb04467449f5542e88
HandsomeWalker <dreamcreativehub@gmail.com>
2018-08-21 11:58:43 +0800

    change private way

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import HandsomeX from './src/HandsomeX';
 var Vue = {};
 var t = new HandsomeX('handsome');
 t.bind(Vue);
-var res = t.set('test', 'jack').clear().get();
-console.log(res);
+var res = t.set('test', 'jack').get();
+console.log(res, Vue);
--- src/HandsomeX.js
+++ src/HandsomeX.js
@@ -1,36 +1,45 @@
-var bind = function(instance) {
-    if(instance[this.name]) {
-        throw new Error('exist name! please use other name...');
-    }else {
-        instance[this.name] = this;
-    }
+let isObject = function(obj) {
+    return Object.prototype.toString.apply(obj) === '[object Object]';
 }
-function Walker(name, has_proto) {
-    var _private;
-    this.has_proto = has_proto;
-    var _init = function() {
-        this.has_proto ? (_private = {}) : (_private = Object.create(null));
-    }
-    _init.apply(this);
-    var get = function(key) {
-        if(key && typeof key === 'string') {
-            return _private[key];
-        }else {
-            return _private;
-        }
-    }
-    var set = function(key, value, options) {
-        _private[key] = value;
-        return this;
-    }
-    var clear = function() {
-        _init.apply(this);
-        return this;
-    }
+/**
+ *
+ * @param {string} name
+ */
+function Walker(name) {
     this.name = name;
-    this.bind = bind;
-    this.set = set;
-    this.get = get;
-    this.clear = clear;
 }
+Walker.prototype = (function() {
+    let _private = Object.create(null);
+    return {
+        isObject: isObject,
+        bind: function(instance) {
+            if(instance[this.name]) {
+                throw new Error('existing name! please use other name...');
+            }else {
+                instance[this.name] = this;
+            }
+            return this;
+        },
+        get: function(key) {
+            if(key && typeof key === 'string') {
+                return _private[key];
+            }else {
+                return _private;
+            }
+        },
+        set: function(key, value, options) {
+            let opts = {
+                mode: 'scope',
+                expire: ''
+            };
+            this.isObject(options) && (Object.assign(opts, options));
+            _private[key] = value;
+            return this;
+        },
+        clear: function() {
+            _private = Object.create(null);
+            return this;
+        },
+    };
+}());
 export default Walker;
